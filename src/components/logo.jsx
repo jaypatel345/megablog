2f56bbe8efa938db8108b70b5ea0d60087542f61
Jay patel <77409923+jaypatel345@users.noreply.github.com>
2025-07-11 22:11:21 +0530

    final project

---
+++ src/components/logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import logo from "/Users/Jaypatel/Downloads/Project/Megablog/megablog/src/download-removebg-preview.png"
+
+function Logo({}) {
+  return (
+    <img src={logo} alt="Logo" width = '100px'  />
+  )
+}
+
+export default Logo
